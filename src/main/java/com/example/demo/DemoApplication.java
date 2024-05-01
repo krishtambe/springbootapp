70c45acd246bac7b46ef39ee7ca9d4a59f565de8
Krish Tambe <krish.tambe@gmail.com>
2024-05-01 22:03:24 +0530

    init commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+@Controller
+class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+}
